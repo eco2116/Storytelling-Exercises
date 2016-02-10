4372601e2399e583be420edf092d6b7da6ba0dba
evanoconnor <eco2116@columbia.edu>
2016-02-10 15:33:26 -0500

    got location filter working

--- server.py
+++ server.py
@@ -16,59 +16,9 @@ consumer_secret = "yUxhzsWXn1TSCzkIEeUIHQKCZyI7PabLaNeFSDLwC2voGg9irV"
 
 auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
 stream = TwitterStream(auth=auth)
-it = stream.statuses.filter(track='water')
+it = stream.statuses.filter(track='safe water', locations='43,-84,44,-83')
 
 for t in it:
 	print json.dumps(t)
 	sleep(5)
 
-# auth = OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
-# stream = Stream(auth, l)
-# stream_SFO = stream.filter(locations=[-122.75,36.8,-121.75,37.8], track=['python'],
-# 	language="en")
-
-# class StdOutListener(StreamListener):
-
-# 	def __init__(self, api=None):
-# 	    super(StdOutListener, self).__init__()
-# 	    self.tweet_array = []
-# 	    self.tweet_count = 0
-
-# 	def on_status(self, status):
-# 		record = {'Tweet text': status.text, 'Tweet time': status.created_at}
-#         	print record
-#         	sleep(8)
-
-# PORT_NUMBER = 8111
-
-# #This class will handles any incoming request from
-# #the browser 
-# class myHandler(BaseHTTPRequestHandler):
-	
-# 	#Handler for the GET requests
-# 	def do_GET(self):
-# 		self.send_response(200)
-# 		self.send_header('Content-type','text/html')
-# 		self.end_headers()
-
-# 		#This handles Twitter authetification and the connection to Twitter Streaming API
-# 		l = StdOutListener()
-		
-		
-# 		#This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-# 		return
-
-# try:
-# 	#Create a web server and define the handler to manage the
-# 	#incoming request
-# 	server = HTTPServer(('', PORT_NUMBER), myHandler)
-# 	print 'Started httpserver on port ' , PORT_NUMBER
-	
-# 	#Wait forever for incoming htto requests
-# 	server.serve_forever()
-
-# except KeyboardInterrupt:
-# 	print '^C received, shutting down the web server'
-# 	server.socket.close()
-	
