ee3bd9b7030108a1e77475aba202fdfac69760cd
Evan O'Connor <eco2116@columbia.edu>
2016-03-16 19:10:42 -0400

    More comments

--- exercise3/morning.py
+++ exercise3/morning.py
@@ -1,3 +1,25 @@
+#########################################
+# Evan O'Connor	
+# eco2116
+# Storytelling and Streaming Data  
+# exercise 3
+# morning.py 
+#########################################
+
+# This script tracks tweets that either mention good morning or good
+# morning. It prints a JSON dump of the tweet, the language of the tweet,
+# and a "time", which is either night or morning, in this case morning.
+# count.py will read in from this JSON dump, notice that morning was chosen,
+# and update the redis DB accordingly. Capturing a data point from this
+# stream could possibly indicate to us that people are going to bed
+# in the given location. While the language does not explicitly imply
+# the geographic location of users, it is our best indicator, as all
+# other twitter geolocators are rarely included in Twitter's response.
+# While the distribution of morning/night may be difficult to understand
+# in a geographic sense by using something such as lanugage, it provides
+# us with a general approach to the problem, and in the future, if Tweets
+# start containing more geographic details, we could implement it as such.
+
 import json
 from time import sleep
 from twitter import OAuth, Twitter, TwitterStream
--- exercise3/night.py
+++ exercise3/night.py
@@ -2,16 +2,23 @@
 # Evan O'Connor	
 # eco2116
 # Storytelling and Streaming Data  
-# exercise 2
+# exercise 3
 # night.py 
 #########################################
 
-# This script takes in a language as a command line argument. It then
-# connects to redis, assuming that the morning/night scripts and the
-# count scripts have been run (see README for use). It then tracks
-# the entropy of the system and the probability of receiving a good morning
-# or goodnight tweet for the specified language. It will send a tweet
-# to my account of any of these values are unusually high.
+# This script tracks tweets that either mention good morning or good
+# night. It prints a JSON dump of the tweet, the language of the tweet,
+# and a "time", which is either day or night, in this case night.
+# count.py will read in from this JSON dump, notice that night was chosen,
+# and update the redis DB accordingly. Capturing a data point from this
+# stream could possibly indicate to us that people are going to bed
+# in the given location. While the language does not explicitly imply
+# the geographic location of users, it is our best indicator, as all
+# other twitter geolocators are rarely included in Twitter's response.
+# While the distribution of night/day may be difficult to understand
+# in a geographic sense by using something such as lanugage, it provides
+# us with a general approach to the problem, and in the future, if Tweets
+# start containing more geographic details, we could implement it as such.
 
 import json
 from time import sleep
